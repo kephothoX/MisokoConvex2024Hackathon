2fd1d3366f7be9db69401aef905d0afa71ec875e
Kephotho <kephothosolutions@gmail.com>
2024-08-12 17:15:07 +0300

    Fixed Header.component

--- MISoko_UX/src/app/components/header/header.component.ts
+++ MISoko_UX/src/app/components/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from '@auth0/auth0-angular';
-
 import { ActivatedRoute, Router } from '@angular/router';
 //import { MISokoService } from 'src/app/sNIndustry/MISoko.service';
 
@@ -30,33 +28,11 @@ export class HeaderComponent implements OnInit {
     private _router: Router,
     //private _MISokoService: MISokoService,
     public _snackBar: MatSnackBar,
-    private _auth0Service: AuthService,
     public _matSnackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
-    if (window.sessionStorage.getItem('isAuthenticated') == null || undefined ||  'undefined'|| 'false' || false && window.sessionStorage.getItem('userId') == null || undefined) {
-      this._auth0Service.isAuthenticated$.subscribe((response: any) => {
-
-        window.sessionStorage.setItem('isAuthenticated', `${response}`);
-      });        
-    }
-    this.title = this._activatedRoute.snapshot.routeConfig?.title?.toString();
-    /*if (window.sessionStorage.getItem('profileId') != null || undefined || 'undefined') {
-      this._MISokoService.getUserProfileByEmail({ email: window.sessionStorage.getItem('email') }).subscribe((response: any) => {
-
-        if (response.length >= 1) {
-          this.userProfile = response[0];
-          this.profileId = response[0]._id;
-
-          if (response[0].profile_thumbnail != null) {
-            this.imageURL = response[0].profile_thumbnail;
-          } else {
-            this.imageURL = response[0].social_image_url;
-          }
-        }
-      });
-    }*/
+    
   }
 
 
